bd036b389b372a785119720fe495ec1223d8c618
dzming <dzm_work@163.com>
2024-01-03 11:35:34 +0000

    添加image_renderer服务端的框架（未实现）

---
+++ src/grpc/servers/image_renderer/image_renderer_server.cpp
@@ -0,0 +1,29 @@
+#include "grpc/servers/image_renderer/image_renderer_server.h"
+#include <chrono>
+#include <random>
+#include <unordered_map>
+#include "task_manager/task_manager.h"
+
+auto taskManager = TaskManager::getSingletonInstance();
+
+ImageRendererServer::ImageRendererServer() {
+}
+
+ImageRendererServer::~ImageRendererServer() {
+}
+
+grpc::Status ImageRendererServer::getLatestImageByImageId(grpc::ServerContext*, const imageRenderer::GetImageByImageIdRequest *request, imageRenderer::GetImageByImageIdResponse *response) {
+    int32_t responseCode = 200;
+    std::string responseMessage;
+    int64_t taskId = 0;
+    request->PrintDebugString();
+    try {
+
+    } catch (const std::exception& e) {
+        responseCode = 400;
+        responseMessage += e.what();
+    }
+    response->mutable_response()->set_code(responseCode);
+    response->mutable_response()->set_message(responseMessage);
+    return grpc::Status::OK;
+}
---
+++ src/grpc/servers/image_renderer/image_renderer_server.h
@@ -0,0 +1,31 @@
+/*****************************************************************************
+*  Copyright © 2024 - 2024 dzming.                                           *
+*                                                                            *
+*  @file     image_renderer_server.h                                         *
+*  @brief    gRPC impl: image renderer                                       *
+*  @author   dzming                                                          *
+*  @email    dzm_work@163.com                                                *
+*                                                                            *
+*----------------------------------------------------------------------------*
+*  Remark  :  proto file: resources/protos/image_renderer.proto              *
+*****************************************************************************/
+
+#ifndef _IMAGE_RENDERER_SERVER_H_
+#define _IMAGE_RENDERER_SERVER_H_
+
+#include <string>
+#include "protos/image_renderer/image_renderer.grpc.pb.h"
+#include "protos/image_renderer/image_renderer.pb.h"
+#include "grpc/servers/grpc_server.h"
+#include "grpc/servers/grpc_server_builder.h"
+
+class ImageRendererServer: public imageRenderer::Communicate::Service {
+public:
+    ImageRendererServer();
+    virtual ~ImageRendererServer();
+
+    virtual grpc::Status getLatestImageByImageId(grpc::ServerContext*, const imageRenderer::GetImageByImageIdRequest*, imageRenderer::GetImageByImageIdResponse*) override;
+private:
+};
+
+#endif /* _IMAGE_RENDERER_SERVER_H_ */
